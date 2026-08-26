a5ebc57e533cf7ba86028056de5a4bb77677e118
Aman Yadav <yadav.amanyadav183@gmail.com>
2026-08-27 03:10:41 +0530

    Add process and thread basics

--- ProcessDemo.java
+++ ProcessDemo.java
@@ -2,6 +2,7 @@ public class ProcessDemo {
     public static void main(String[] args) throws Exception {
         long pid = ProcessHandle.current().pid();
         System.out.println("The Current PID : " + pid);
+        System.out.println("Main Thread : " + Thread.currentThread().getName());
 
         while(true){
             Thread.sleep(1000);
